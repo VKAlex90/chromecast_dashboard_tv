4348ef6d5e2ec99c033d1c94dbf16477b32511e5
VKAlex90 <vk.alexandr@gmail.com>
2017-01-30 12:34:40 +0200

    Update main.js

--- receiver/main.js
+++ receiver/main.js
@@ -9,7 +9,7 @@ window.onload = function() {
 
   castReceiverManager.onReady = function(event) {
     console.log('Received Ready event: ' + JSON.stringify(event.data));
-    window.castReceiverManager.setApplicationState('chromecast-dashboard is ready...');
+    window.castReceiverManager.setApplicationState('chromecast-dashboard tv is ready...');
   };
 
   castReceiverManager.onSenderConnected = function(event) {
@@ -22,7 +22,7 @@ window.onload = function() {
 
   window.messageBus =
     window.castReceiverManager.getCastMessageBus(
-        'urn:x-cast:com.boombatower.chromecast-dashboard', cast.receiver.CastMessageBus.MessageType.JSON);
+        'urn:x-cast:com.test.chromecast-dashboard-tv', cast.receiver.CastMessageBus.MessageType.JSON);
 
   window.messageBus.onMessage = function(event) {
     console.log('Message [' + event.senderId + ']: ' + event.data);
